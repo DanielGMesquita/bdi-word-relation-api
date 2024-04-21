f3b532b28e551e0fee23ea748be765549cad1535
Daniel Mesquita <daniel.mesquita@blip.ai>
2024-04-21 01:06:14 -0300

    refactor: set correct service

--- src/main/java/br/com/daniel/biwordrelation/service/DictionaryScraperService.java
+++ src/main/java/br/com/daniel/biwordrelation/service/DictionaryScraperService.java
@@ -1,5 +1,8 @@
 package br.com.daniel.biwordrelation.service;
 
+import br.com.daniel.biwordrelation.dto.DictionaryScraperResponseDTO;
+import br.com.daniel.biwordrelation.entities.DictionaryScraperRequest;
+import br.com.daniel.biwordrelation.manager.DictionaryManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,8 +11,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class DictionaryScraperService {
+  private final DictionaryManager dictionaryManager;
 
-  public void scrapeDictionary(String word) {
-    log.info("Scraping dictionary for word: {}", word);
+  public DictionaryScraperResponseDTO scrapeDictionaryForSynonyms(
+      DictionaryScraperRequest request) {
+    log.info("Scraping dictionary for word: {}", request.getWord());
+    return this.dictionaryManager.getSynonyms(request);
   }
 }
