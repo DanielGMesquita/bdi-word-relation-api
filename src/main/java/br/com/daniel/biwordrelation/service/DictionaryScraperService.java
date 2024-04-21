67ca0ba9ac1e5539cee2de4e197702c81b2aa4b9
Daniel Mesquita <daniel.mesquita@blip.ai>
2024-04-21 01:00:57 -0300

    feat: create service class

--- src/main/java/br/com/daniel/biwordrelation/service/DictionaryScraperService.java
+++ src/main/java/br/com/daniel/biwordrelation/service/DictionaryScraperService.java
@@ -1,3 +1,15 @@
 package br.com.daniel.biwordrelation.service;
 
-public class DictionaryScraperService {}
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DictionaryScraperService {
+
+  public void scrapeDictionary(String word) {
+    log.info("Scraping dictionary for word: {}", word);
+  }
+}
