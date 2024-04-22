630c47b0dbc5d525b4f843ca621cf563ea2f1d46
Daniel Mesquita <daniel.mesquita@blip.ai>
2024-04-22 19:24:18 -0300

    refactor: separate responsabilities

--- src/main/java/br/com/daniel/biwordrelation/manager/DictionaryManager.java
+++ src/main/java/br/com/daniel/biwordrelation/manager/DictionaryManager.java
@@ -26,60 +26,50 @@ public class DictionaryManager {
   public DictionaryScraperSynonymResponseDTO getSynonyms(
       DictionaryScraperRequest dictionaryScraperRequest)
       throws ErrorGettingPageDataException, NoContentException {
-    Element contentList = this.pageScraper.getPageData(dictionaryScraperRequest);
+    Element contentList = getPageContent(dictionaryScraperRequest);
     String baseWord = dictionaryScraperRequest.getWord();
 
     if (contentList == null) {
       throw new NoContentException("No content found for the word provided.");
     }
 
+    List<RootNodeEntity> rootChildren =
+        extractRootChildren(contentList, baseWord, dictionaryScraperRequest);
+
+    setWordCountForChildNodes(rootChildren);
+
+    NodeEntity root = new NodeEntity(dictionaryScraperRequest.getWord(), rootChildren);
+    DictionaryScraperSynonymResponseDTO responseDTO = new DictionaryScraperSynonymResponseDTO();
+    responseDTO.setSynonymsTree(root);
+
+    return responseDTO;
+  }
+
+  private Element getPageContent(DictionaryScraperRequest dictionaryScraperRequest)
+      throws ErrorGettingPageDataException {
+    return this.pageScraper.getPageData(dictionaryScraperRequest);
+  }
+
+  private List<RootNodeEntity> extractRootChildren(
+      Element contentList, String baseWord, DictionaryScraperRequest dictionaryScraperRequest)
+      throws ErrorGettingPageDataException {
+    List<RootNodeEntity> rootChildren = new ArrayList<>();
     Elements items = contentList.getElementsByTag("li");
 
-    List<RootNodeEntity> rootChildren = new ArrayList<>();
-
     for (Element item : items) {
-      List<String> rootChildrenList = new ArrayList<>();
-      Element paragraph = item.getElementsByTag("p").first();
-
-      List<Node> words = new ArrayList<>();
-      if (paragraph != null) {
-        words.addAll(paragraph.childNodes());
-      }
-
-      HtmlElementProcessor.populateSynonymsList(
-          words, baseWord, dictionaryScraperRequest, rootChildrenList);
+      List<String> rootChildrenList = processWordsToList(item, baseWord, dictionaryScraperRequest);
 
       allWords.addAll(rootChildrenList);
 
       for (String synonym : rootChildrenList) {
         DictionaryScraperRequest synonymRequest = new DictionaryScraperRequest();
         synonymRequest.setWord(synonym);
-        Element synonymElementList = this.pageScraper.getPageData(synonymRequest);
+        Element synonymElementList = getPageContent(synonymRequest);
 
         Elements childItems = synonymElementList.getElementsByTag("li");
 
-        List<ChildrenNodeEntity> clusterChildren = new ArrayList<>();
-
-        for (Element rootItem : childItems) {
-          List<String> clusterChildrenList = new ArrayList<>();
-          Element rootParagraph = rootItem.getElementsByTag("p").first();
-
-          List<Node> rootWords = new ArrayList<>();
-          if (rootParagraph != null) {
-            rootWords.addAll(rootParagraph.childNodes());
-          }
-
-          HtmlElementProcessor.populateSynonymsList(
-              rootWords, baseWord, synonymRequest, clusterChildrenList);
-          allWords.addAll(clusterChildrenList);
-
-          for (String clusterWord : clusterChildrenList) {
-            ChildrenNodeEntity clusterNode = new ChildrenNodeEntity(clusterWord, 0);
-            if (!clusterChildren.contains(clusterNode)) {
-              clusterChildren.add(clusterNode);
-            }
-          }
-        }
+        List<ChildrenNodeEntity> clusterChildren =
+            populateClusterChildren(childItems, baseWord, dictionaryScraperRequest);
 
         RootNodeEntity rootChild = new RootNodeEntity(synonym, clusterChildren);
         if (!rootChildren.contains(rootChild)) {
@@ -87,18 +77,49 @@ public class DictionaryManager {
         }
       }
     }
+    return rootChildren;
+  }
 
+  private List<ChildrenNodeEntity> populateClusterChildren(
+      Elements childItems, String baseWord, DictionaryScraperRequest dictionaryScraperRequest) {
+    List<ChildrenNodeEntity> clusterChildren = new ArrayList<>();
+    for (Element rootItem : childItems) {
+      List<String> clusterChildrenList =
+          processWordsToList(rootItem, baseWord, dictionaryScraperRequest);
+      allWords.addAll(clusterChildrenList);
+
+      for (String clusterWord : clusterChildrenList) {
+        ChildrenNodeEntity clusterNode = new ChildrenNodeEntity(clusterWord, 0);
+        if (!clusterChildren.contains(clusterNode)) {
+          clusterChildren.add(clusterNode);
+        }
+      }
+    }
+    return clusterChildren;
+  }
+
+  private List<String> processWordsToList(
+      Element item, String baseWord, DictionaryScraperRequest dictionaryScraperRequest) {
+    List<String> childrenList = new ArrayList<>();
+    Element paragraph = item.getElementsByTag("p").first();
+
+    List<Node> words = new ArrayList<>();
+    if (paragraph != null) {
+      words.addAll(paragraph.childNodes());
+    }
+
+    HtmlElementProcessor.populateSynonymsList(
+        words, baseWord, dictionaryScraperRequest, childrenList);
+
+    return childrenList;
+  }
+
+  private void setWordCountForChildNodes(List<RootNodeEntity> rootChildren) {
     for (RootNodeEntity clusterNode : rootChildren) {
       for (ChildrenNodeEntity clusterChild : clusterNode.getChildren()) {
         String nodeName = clusterChild.getName();
         clusterChild.setValue((int) allWords.stream().filter(str -> str.equals(nodeName)).count());
       }
     }
-
-    NodeEntity root = new NodeEntity(dictionaryScraperRequest.getWord(), rootChildren);
-    DictionaryScraperSynonymResponseDTO responseDTO = new DictionaryScraperSynonymResponseDTO();
-    responseDTO.setSynonymsTree(root);
-
-    return responseDTO;
   }
 }
